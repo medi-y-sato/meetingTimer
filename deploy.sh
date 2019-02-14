4fc0a214d0880316ec12eb87203bd95e6a721274
Yoshiaki Satoh <y-sato@mediba.jp>
2019-02-14 18:29:52 +0900

    ビルドと出力ディレクトリの修正

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eu
-cd docs
-rm -f meetingtimer_deploy_key meetingtimer_deploy_key.enc deploy.sh .travis.yml package.json
+cd dist
 git init
 git add .
 git commit -m "Publishing site on `date "+%Y-%m-%d %H:%M:%S"`"
